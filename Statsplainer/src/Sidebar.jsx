ce5a5321946685b64d8d71ddb9855a06d424b2c3
Luke Chen <lukechen@Lukes-MacBook-Pro.local>
2025-03-12 16:56:01 +1100

    added message chat box section

--- Statsplainer/src/Sidebar.jsx
+++ Statsplainer/src/Sidebar.jsx
@@ -1,8 +1,10 @@
+import { TextField } from '@mui/material';
 import Grid from '@mui/material/Grid2';
 import {useState} from "react";
 
 // Sidebar Function
 
+//update here where the border of the side bar starts
 const NAVBAR_HEIGHT = 60;
 
 export default function Sidebar() {
@@ -10,17 +12,33 @@ export default function Sidebar() {
     return (
         <Grid
             container
-        sx={{
-            position: "fixed",
-            right:30,
-            top: NAVBAR_HEIGHT,
-            height:"90vh",
-            width: 400,
-            borderRadius: "20px",
-            backgroundColor: "#464646",
-            flexDirection: "column",
-        }}
+            sx={{
+                position: "fixed",
+                right:30,
+                top: NAVBAR_HEIGHT,
+                height:"90vh",
+                width: 400,
+                borderRadius: "20px",
+                backgroundColor: "#464646",
+                flexDirection: "column",
+            }}
         >
+        {/* response section */}
+
+
+        
+        {/* chat box input section */}
+        <Grid item 
+            sx={{
+                backgroundColor: "#ddd",
+                borderRadius : "10px",
+            }}>            
+            <TextField
+                fullWidth
+                placeholder="Ask Anything"
+                variant="outlined"
+            />
+        </Grid>
 
 
         </Grid>
