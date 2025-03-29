e7dc4602210de5895c2706ffab138ce0c6e8c036
LC731816289 <lukechen731816289@gmail.com>
2025-03-29 14:39:38 +1100

    added dismiss button and message in tooltip bubble

--- Statsplainer/frontend/src/Tooltips.jsx
+++ Statsplainer/frontend/src/Tooltips.jsx
@@ -1,5 +1,6 @@
 import { Box, Button, Paper, TextField, Snackbar, Slide, Typography } from '@mui/material';
 import {useState, useRef, useEffect} from "react";
+import Grid from '@mui/material/Grid2';
 
 // tooltip bubble function that create a rectangular bubble with a
 // dismiss button to guide through the user what each component does and how to interact with.
@@ -10,19 +11,52 @@ export default function Tooltip ({targetRef, open, onClose}) {
         <Snackbar
             open={open}
             onClose={onClose}
-            anchorOrigin={{vertical:"top", horizontal: "right"}}
-            message={
-                <Box
-                >
-                    <Typography>
-                        tooltip message !!!!
-                    </Typography>
+            anchorOrigin={{vertical:"top", horizontal: "left"}}
+            sx={{
+                backgroundColor: "grey",
+                borderRadius: "10px",
+                minHeight: "10vh",
+                width: "25vw",
+                p:2,
+            }}
 
-                </Box>
-            }
+                       
         >
-            
+            <Grid
 
+                
+                sx={{
+                    minWidth: "auto",
+                    minHeight: "auto",
+                    alignItems: "flex-start",
+                }}
+            
+            >
+                {/* tooltips message */}
+                <Typography 
+                    sx={{
+                        wordBreak: 'break-word',
+                        pb:1,
+                    }}
+                >
+                    tooltip message !!!! will change to a variabledwa dawd ad wdw wdaiodaoidkioa dwaidjoawd
+                </Typography>
+
+                {/* Dismiss button */}
+                <Button
+                    size="small"
+                    onClick={onClose}
+                    
+                    sx={{
+                        backgroundColor:"lightblue",
+                        alignSelf:"flex-end",
+                        }}
+                >
+                    Dismiss
+
+                </Button>
+                
+            </Grid>
         </Snackbar>
     )
 }
