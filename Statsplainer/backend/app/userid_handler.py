76a4938edb97bd43f7919ffef5e7ba9ef3ff159f
Benny <benny.wang3009@gmail.com>
2025-04-29 20:32:48 +1000

    Removed unused code block

--- Statsplainer/backend/app/userid_handler.py
+++ Statsplainer/backend/app/userid_handler.py
@@ -1,27 +1,12 @@
-from flask import jsonify, make_response, request, Blueprint
-from uuid import uuid4
+from flask import jsonify, request, Blueprint
 from util import pass_to_google_forms
 
 userid_handler = Blueprint("cookie_handler", __name__)
 
-""" @cookie_handler.route('/set-cookie')
-def set_cookie():
-    if 'uid' not in request.cookies:
-        uid = str(uuid4())
-        resp = make_response(f"Set new browser_id. Value: {uid}")
-        resp.set_cookie(
-            'uid', 
-            uid, 
-            path='/', 
-            max_age=60*60*24*365,
-            httponly='True',
-            samesite='Lax'
-            )
-        return resp
-    return f"Cookie already set. Value: {request.cookies.get('uid')}"  """
-
+# endpoint for handling user id
 @userid_handler.route('/user_id', methods=["POST"])
 def get_id():
+    #obtain user id from frontend and send to google forms
     user_id = request.cookies.get('user_id')
     if user_id == "null":
         user_id = "No user_id found."
